e55090f9183d66ae3680fe8b8856f84ec61b8531
JohnAMcEwan <124451725+JohnAMcEwan@users.noreply.github.com>
2023-02-25 16:44:54 +0000

    I dont know

--- main.swift
+++ main.swift
@@ -1,1 +1,28 @@
-print("test1")
+// var num = 10
+// print(num)
+// if num == 20
+// {
+// print(num)
+  
+// }
+// else {
+// print("20")
+  
+// }
+
+import Foundation
+import Glibc
+
+print("Please enter number 1")
+var n = Int(readLine()!)!
+
+switch n {
+case 1:
+    print("It's 1!")
+case 2...4:
+    print("It's between 2 and 4!")
+case 5, 6:
+    print("It's 5 or 6")
+default:
+    print("Its another number!")
+}
