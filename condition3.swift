5e9b9d71cec14d1787a4c346eed9a8e65933b3b9
JohnAMcEwan <124451725+JohnAMcEwan@users.noreply.github.com>
2023-03-19 21:35:14 +0000

    This is condition2.swift.  It improves over condition.swift uploaded last night in that it consistently returns the first character that repeats the most of any inputted string of any length of any random order of characters.  It achieves this by selecting those characters with an index lower (< vice ==) than that of the characters with the highest repetition, that is among those having the maxCount (== vice >).  Also this version accommodates the readLine and if (using let if) commands more completely and dispenses with loading the Foundation and Glibc libraries since only Swift built-in functions and types are used.

--- condition3.swift
+++ condition3.swift
@@ -5,5 +5,9 @@ for char in str {if let count = lib[char] {lib[char] = count + 1} else {lib[char
 var maxChar: Character = " "
 var maxCount = 0
 for (char,count) in lib {if count > maxCount {maxChar = char; maxCount = count}}
+var ind = 0
+for (_,count) in lib {if count == maxCount {ind = ind + 1}}
+print(ind)
+  
 for (char,count) in lib {if count == maxCount && str.firstIndex(of: char)! < str.firstIndex(of: maxChar)! {maxChar = char}}
 print(maxChar)}
